f3d0cade777decb7342a65ef15b2d96a5a27ee6e
xstater <a166224@163.com>
2018-11-10 02:04:37 +0800

    更新了group

--- Group.hpp
+++ Group.hpp
@@ -1,11 +1,26 @@
 #ifndef _XTASK_GROUP_H_
 #define _XTASK_GROUP_H_
 
-template<class TaskType>
+#include <mutex>
+#include <array>
+#include <bitset>
+#include "XSignal.hpp"
+
+template<class TaskType,int N>
 class Group{
 public:
+    Group();
+    ~Group();
+    
+    void add(TaskType &&task);
+    void run();
+    
+    Signal<void()> onComplete;
 protected:
 private:
+    std::mutex m_mutex;
+    std::bitset<N> m_bits;
+    std::array<TaskType,N> m_tasks;
 };
 
 #endif //_XTASK_GROUP_H_
