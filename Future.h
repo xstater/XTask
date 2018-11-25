3966e915e7fb21a982f75f0ce6483c1ec398b874
xstater <a166224@163.com>
2018-11-26 01:31:10 +0800

    完成了Future类和Promise类

--- Future.h
+++ Future.h
@@ -7,44 +7,155 @@
 #include "ThreadPool.h"
 
 namespace xtask{
+    enum class Policy{
+        pool,
+        thread,
+        synchronized
+    };
+
+    enum class Status{
+        waiting,
+        running,
+        done
+    };
+
     template <class Type>
     struct FutureBase{
         FutureBase()
             :m_data(),
-             m_done(false),
+             m_status(Status::waiting),
              m_exception(nullptr),
-             m_then(nullptr){}
+             m_then(nullptr),
+             m_then_policy(Policy::pool){}
         ~FutureBase() = default;
 
         Type m_data;
-        bool m_done;
+        Status m_status;
         std::exception_ptr m_exception;
         std::function<void()> m_then;
+        Policy m_then_policy;
+    };
+
+    template <>
+    struct FutureBase<void>{
+        FutureBase()
+                :m_status(Status::waiting),
+                 m_exception(nullptr),
+                 m_then(nullptr),
+                 m_then_policy(Policy::pool){}
+        ~FutureBase() = default;
+
+        Status m_status;
+        std::exception_ptr m_exception;
+        std::function<void()> m_then;
+        Policy m_then_policy;
     };
 
     template <class Type>
     class Future{
     public:
-        Future(std::shared_ptr<FutureBase<Type>> fut);
-        Future(const Future &);
-        Future(Future &&);
-        ~Future();
+        Future(std::shared_ptr<FutureBase<Type>> fut)
+            :m_future(std::move(fut)){}
+        Future(const Future &) = default;
+        Future(Future &&) = default;
+        ~Future() = default;
 
-        Future &operator=(const Future &);
-        Future &operator=(Future &&);
+        Future &operator=(const Future &) = default;
+        Future &operator=(Future &&) = default;
 
-        Type get();
-        void wait();
+        Type get()const{
+            wait();
+            if(m_future->m_exception)std::rethrow_exception(m_future->m_exception);
+            return m_future->m_data;
+        }
+        void wait()const noexcept{
+            while(m_future->m_status != Status::done && !m_future->m_exception){}
+        }
 
         template <class Function>
-        auto then(Function &&function) -> Future<return_type<Function>>{
-            m_future->m_then = []()->void{
-                
+        auto then(Function &&function,Policy policy = Policy::pool) -> Future<return_type_t<Function>>{
+            auto ptr = std::make_shared<FutureBase<return_type_t<Function>>>();
+            m_future->m_then_policy = policy;
+            m_future->m_then = [ptr,function,this]()->void{
+                try{
+                    ptr->m_status = Status::running;
+                    ptr->m_data = function(Future<Type>(*this));
+                    ptr->m_status = Status::done;
+                }catch(...){
+                    ptr->m_exception = std::current_exception();
+                }
+                if(ptr->m_then){
+                    switch(ptr->m_then_policy){
+                        case Policy::pool:
+                            ThreadPool::instance().addTask(ptr->m_then);
+                            break;
+                        case Policy::thread:
+                            std::async(std::launch::async,ptr->m_then);
+                            break;
+                        case Policy::synchronized:
+                            ptr->m_then();
+                            break;
+                    }
+                }
             };
+            return Future<return_type_t<Function>>(ptr);
         }
     protected:
     private:
         std::shared_ptr<FutureBase<Type>> m_future;
+    };
+
+    template <>
+    class Future<void>{
+    public:
+        Future(std::shared_ptr<FutureBase<void>> fut)
+                :m_future(std::move(fut)){}
+        Future(const Future &) = default;
+        Future(Future &&) = default;
+        ~Future() = default;
+
+        Future &operator=(const Future &) = default;
+        Future &operator=(Future &&) = default;
+
+        void get()const{
+            wait();
+            if(m_future->m_exception)std::rethrow_exception(m_future->m_exception);
+        }
+        void wait()const noexcept{
+            while(m_future->m_status != Status::done && !m_future->m_exception){}
+        }
+
+        template <class Function>
+        auto then(Function &&function,Policy policy = Policy::pool) -> Future<return_type_t<Function>>{
+            auto ptr = std::make_shared<FutureBase<return_type_t<Function>>>();
+            m_future->m_then_policy = policy;
+            m_future->m_then = [ptr,function,this]()->void{
+                try{
+                    ptr->m_status = Status::running;
+                    function(Future<void>(*this));
+                    ptr->m_status = Status::done;
+                }catch(...){
+                    ptr->m_exception = std::current_exception();
+                }
+                if(ptr->m_then){
+                    switch(ptr->m_then_policy){
+                        case Policy::pool:
+                            ThreadPool::instance().addTask(ptr->m_then);
+                            break;
+                        case Policy::thread:
+                            std::async(std::launch::async,ptr->m_then);
+                            break;
+                        case Policy::synchronized:
+                            ptr->m_then();
+                            break;
+                    }
+                }
+            };
+            return Future<return_type_t<Function>>(ptr);
+        }
+    protected:
+    private:
+        std::shared_ptr<FutureBase<void>> m_future;
     };
 }
 
