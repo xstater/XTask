9901cd07ee202d2bf82bd253d4154043bbcb68cf
xstater <a166224@163.com>
2018-11-26 18:57:53 +0800

    添加了汇总头文件

---
+++ XTask.hpp
@@ -0,0 +1,11 @@
+#ifndef _XTASK_HPP_
+#define _XTASK_HPP_
+
+#include "return_type.h"
+#include "ThreadPool.h"
+#include "Future.h"
+#include "Promise.h"
+#include "Task.h"
+#incldue "when.h"
+
+#endif
