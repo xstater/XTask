f839dee9c6f461540c1427f86bfee22a4aae45e7
xstater <a166224@163.com>
2018-11-26 15:51:23 +0800

    修改了when的实现方法

--- when.h
+++ when.h
@@ -2,34 +2,16 @@
 #define _XTASK_WHEN_H_
 
 #include <memory>
+#include <iterator>
 #include <initializer_list>
 #include <atomic>
 #include "Future.h"
 
 namespace xtask{
-    template <class Type>
-    class when_all{
-    public:
-        template <class Iterator>
-        when_all(Iterator beg,Iterator ed);
-        when_all(std::initializer_list<Future<Type>> list);
-
-        Future<void> get_future()const noexcept{
-            
-        }
-    protected:
-    private:
-        std::shared_ptr<FutureBase<void>> m_future;
-        std::atomic<int> m_counter;
-    };
-    
-    template <class Type>
-    class when_any{
-    public:
-    protected:
-    private:
-        std::atomic<bool> m_done;
-    };
+    template<class Iterator>
+    auto when_all() -> std::iterator_traits{
+        
+    }
 }
 
 #endif //_XTASK_WHEN_H_
